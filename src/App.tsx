d11950c2c0bb64753d80a30a5d292f89910caabc
Oliver Zott <zott_oliver@web.de>
2022-01-27 07:08:32 +0100

    Login WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Dashboard from "./pages/Dashboard";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./pages/Users";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-
-                <Routes>
-                    <Route path="/" element={<Dashboard/>}/>
-                    <Route path="/users" element={<Users/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                </Routes>
-
-            </BrowserRouter>
-        </div>
-    );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Dashboard />} />
+					<Route path="/users" element={<Users />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+    const [count, setCount] = useState(0);
+
     return (
-        <div>
-            Login
+        <div className="container">
+            <h1>Login</h1>
+            Count = {count}
+            <br/>
+            <input type={"number"} onChange={e => setCount(parseInt(e.target.value))}/>
         </div>
     )
 }
