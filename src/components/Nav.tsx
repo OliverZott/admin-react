3f7b14edddc06511857d8a9140b564d0b1d20690
Oliver Zott <zott_oliver@web.de>
2022-02-13 16:59:18 +0100

    User model added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { User } from "../models/user";
 
 const Nav = () => {
-    const [user, setUser] = useState({
-        user: '',
-    });
+    const [user, setUser] = useState(new User());
 
 
     useEffect(() => {
@@ -17,8 +16,12 @@ const Nav = () => {
             async () => {
                 const { data } = await axios.get("user", { withCredentials: true });        // credentials are set global as well
 
-                setUser(data);
-                console.log("WOOOORKED =)")
+                setUser(new User(
+                    data.id,
+                    data.first_name,
+                    data.last_name,
+                    data.email,
+                ));
             }
         )();
     }, [])
@@ -35,7 +38,7 @@ const Nav = () => {
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to={"/profile"} className="p-2 text-white" >{user?.user}</Link>
+                <Link to={"/profile"} className="p-2 text-white" >{user.name}</Link>
                 <Link to={"/login"} className="p-2 text-white" onClick={logout}>Sign Out</Link>
             </ul>
         </nav>
